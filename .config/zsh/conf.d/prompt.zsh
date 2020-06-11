363b0b107a8feeb9f6b8cc0d67cb17a73403138b
Valerio Casalino <casalinovalerio.cv@gmail.com>
2020-06-11 15:34:18 +0200

    Slimmed down prompt

--- .config/zsh/conf.d/prompt.zsh
+++ .config/zsh/conf.d/prompt.zsh
@@ -3,32 +3,25 @@ setopt prompt_subst
 zstyle ":vcs_info:git:*" \
     formats "%{$fg[magenta]%}%r/%S (%{$fg_bold[yellow]%}%b%{$fg[magenta]%})"
 
-# Git status
-GIT_PROMPT_PREFIX="%{$fg[magenta]%}["       # prefix
-GIT_PROMPT_SUFFIX="%{$fg[magenta]%}]"       # suffix
-GIT_PROMPT_AHEAD="%{$fg[green]%}+NUM"       # ahead by "NUM"
-GIT_PROMPT_BEHIND="%{$fg[green]%}-NUM"      # behind by "NUM"
-GIT_PROMPT_MERGING="%{$fg_bold[red]%}X"     # merge conflict
-GIT_PROMPT_UNTRACKED="%{$fg_bold[red]%}?"   # untracked files
-GIT_PROMPT_MODIFIED="%{$fg_bold[yellow]%}M" # modified files
-GIT_PROMPT_STAGED="%{$fg_bold[yellow]%}!"   # staged changes
-
 function _git_prompt() {
     local _out=""
     local N_A="$(git log --oneline @{u}.. 2> /dev/null | wc -l | tr -d ' ')"
     local N_B="$(git log --oneline ..@{u} 2> /dev/null | wc -l | tr -d ' ')"
-    local GIT_UNT="$(git ls-files --other --exclude-standard 2> /dev/null)"
-    [ "$N_A" -gt 0 ] && _out=$_out${GIT_PROMPT_AHEAD//NUM/$N_A}
-    [ "$N_B" -gt 0 ] && _out=$_out${GIT_PROMPT_BEHIND//NUM/$N_B}
-    [ "$GIT_UNT" != "" ] && _out=$_out$GIT_PROMPT_UNTRACKED
-    git diff --quiet 2> /dev/null || _out=$_out$GIT_PROMPT_MODIFIED
-    git diff --cached --quiet 2> /dev/null || _out=$_out$GIT_PROMPT_STAGED
-    [ -n $_out ] && echo "$GIT_PROMPT_PREFIX$_out$GIT_PROMPT_SUFFIX%{$reset_color%}"
+    local G_U="$(git ls-files --other --exclude-standard 2> /dev/null)"
+    [ "$N_A" -gt 0 ] && _out="${_out}%{$fg[green]%}+${N_A}"
+    [ "$N_B" -gt 0 ] && _out="${_out}%{$fg[green]%}-${N_B}"
+    [ "$G_U" != "" ] && _out="${_out}%{$fg_bold[red]%}?"
+    git diff --quiet 2> /dev/null || _out="${_out}%{$fg_bold[yellow]%}M"
+    git diff --cached --quiet 2>/dev/null || _out="${_out}%{$fg_bold[yellow]%}!"
+    [ -n $_out ] && echo "%{$fg[magenta]%}[${_out}%{$fg[magenta]%}]"
 }
 
 function _pwd() {
     vcs_info
-    [ -n "$vcs_info_msg_0_" ] && echo -n "${vcs_info_msg_0_/\/. / }$(_git_prompt) " && return 0
+    if [ -n "$vcs_info_msg_0_" ]; then
+        echo -n "${vcs_info_msg_0_/\/. / }$(_git_prompt) "
+        return 0
+    fi
     echo -n "%{$fg[cyan]%}%3~ "
 }
 
@@ -39,7 +32,7 @@ function _vpn() {
         | xargs -I '{}' echo " %{$fg[yellow]%}[{}] "
 }
 
-_status="%(!.%{$fg[magenta]%}>.%{$fg[yellow]%}>)%(?.%{$fg[green]%}>>.%{$fg[red]%}>>)"
-
-PROMPT="\$(_pwd)\$(_vpn)$_status%{$reset_color%} "
+_isroot="%(!.%{$fg[magenta]%}>.%{$fg[yellow]%}>)"
+_status="%(?.%{$fg[green]%}>>.%{$fg[red]%}>>) "
+PROMPT="\$(_pwd)\$(_vpn)${_is_root}${_status}%{$reset_color%}"
 RPROMPT="%(?..%{$fg[red]%}[%?])%{$reset_color%}"
